fd2da6d9ac46c55a27e4245b1750dd9895eefbe9
Ruramai <ruramaimuchenga@gmail.com>
2024-07-01 19:12:12 +0200

    week1

---
+++ pythonwk1.py
@@ -0,0 +1,39 @@
+"""
+This script prints a hello world message to the console.
+It demonstrates the basic structure of a Python script,
+"""
+
+# Print a message to the console
+print("Hello, world!")
+
+
+
+"""
+This script asks the user for their name and prints a personalized greeting message.
+It demonstrates how to use input() to get user input and how to use string formatting for the output.
+"""
+
+# Ask the user for their name
+user_name = input("What is your name? ")
+
+# Print a personalized greeting message
+print(f"Hello, {user_name}!")
+
+
+
+
+
+"""
+This script asks the user to input the names of three different fruits and stores them in variables.
+It then prints each fruit on a separate line.
+"""
+
+# Asking the user for the names of three different fruits
+fruit1 = input("Enter the name of the first fruit: ")
+fruit2 = input("Enter the name of the second fruit: ")
+fruit3 = input("Enter the name of the third fruit: ")
+
+# Print each fruit on a separate line
+print(fruit1)
+print(fruit2)
+print(fruit3)
